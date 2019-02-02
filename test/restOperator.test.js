1b444bddb9c884a38346548e3a8f45e4d9dc007d
lukix <lukix.95@gmail.com>
2019-02-02 18:38:19 +0100

    Add rest operator tests

---
+++ test/restOperator.test.js
@@ -0,0 +1,29 @@
+const reader = require('../src/index')
+
+describe('Rest operator', () => {
+	test('Normal + rest', () => {
+		const data = `
+			1 2 3 4 5
+		`
+		const pattern = `
+			first ...rest
+		`
+		const result = reader(data, pattern)
+	
+		const expectedResult = { first: 1, rest: [2, 3, 4, 5] }
+		expect(result).toStrictEqual(expectedResult)
+	})
+
+	test('Only rest', () => {
+		const data = `
+			1 2 3 4 5
+		`
+		const pattern = `
+			...rest
+		`
+		const result = reader(data, pattern)
+	
+		const expectedResult = { rest: [1, 2, 3, 4, 5] }
+		expect(result).toStrictEqual(expectedResult)
+	})
+})
