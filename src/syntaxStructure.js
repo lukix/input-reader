b3822a5faebe3e448cc767bebf4952bd53a8f32f
lukix <lukix.95@gmail.com>
2019-02-02 23:29:39 +0100

    Change types values to UPPERCASE

--- src/syntaxStructure.js
+++ src/syntaxStructure.js
@@ -1,13 +1,13 @@
-const LINE_TYPE = 'line'
-const EMPTY_LINE_TYPE = 'emptyLine'
-const LINES_ARRAY_TYPE = 'linesArray'
-const ERROR_TYPE = 'error'
-const SYMBOL_TYPE = 'symbol'
-const SYMBOLS_ARRAY_TYPE = 'symbolsArray'
+const LINE_TYPE = 'LINE_TYPE'
+const EMPTY_LINE_TYPE = 'EMPTY_LINE_TYPE'
+const LINES_ARRAY_TYPE = 'LINES_ARRAY_TYPE'
+const ERROR_TYPE = 'ERROR_TYPE'
+const SYMBOL_TYPE = 'SYMBOL_TYPE'
+const SYMBOLS_ARRAY_TYPE = 'SYMBOLS_ARRAY_TYPE'
 const errorMessages = {
-	lastElement: `'...' operator can be only used as the last symbol of the line`,
-	onlyElement: `'...[]' operator must be the only element in the line`,
-	invalidArrayName: `Invalid array name`,
+	LAST_ELEMENT: `'...' operator can be only used as the last symbol of the line`,
+	ONLY_ELEMENT: `'...[]' operator must be the only element in the line`,
+	INVALID_ARRAY_NAME: `Invalid array name`,
 }
 const whitespaceRegex = /[ ,]+/
 
@@ -23,11 +23,11 @@ function parseLine(patternLine) {
 function parseLinesArray(patternLine) {
 	const arrayName = patternLine.substr(3).split('[')[0]
 	if(!isValidPropName(arrayName)) {
-		return { type: ERROR_TYPE, name: errorMessages.invalidArrayName }
+		return { type: ERROR_TYPE, name: errorMessages.INVALID_ARRAY_NAME }
 	}
 	const afterArrayStr = patternLine.split(']')[1]
 	return whitespaceRegex.test(arrayName) || whitespaceRegex.test(afterArrayStr)
-		? { type: ERROR_TYPE, name: errorMessages.onlyElement }
+		? { type: ERROR_TYPE, name: errorMessages.ONLY_ELEMENT }
 		: {
 			type: LINES_ARRAY_TYPE,
 			name: arrayName,
@@ -46,13 +46,13 @@ function parseSingleLine(patternLine) {
 	const symbolObjects = symbols.map((symbol) => (
 		symbol.substr(0, 3) === '...'
 			? !isValidPropName(symbol.substr(3))
-				? { type: ERROR_TYPE, name: errorMessages.invalidArrayName }
+				? { type: ERROR_TYPE, name: errorMessages.INVALID_ARRAY_NAME }
 				: { type: SYMBOLS_ARRAY_TYPE, name: symbol.substr(3) }
 			: { type: SYMBOL_TYPE, name: symbol }
 	))
 	const validatedSymbolObjects = symbolObjects.map((symbol, index, array) => (
 		symbol.type === SYMBOLS_ARRAY_TYPE && index < array.length - 1
-			? { type: ERROR_TYPE, name: errorMessages.lastElement }
+			? { type: ERROR_TYPE, name: errorMessages.LAST_ELEMENT }
 			: symbol
 	))
 	return { type: LINE_TYPE, symbols: validatedSymbolObjects }
